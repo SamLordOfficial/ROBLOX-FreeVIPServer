cb8c29a7729047e785e7ae5e13def5c6969854f1
Samuel Lord <sllord@icloud.com>
2018-03-25 11:05:04 -0500

    Create free-vip-server.lua

---
+++ free-vip-server.lua
@@ -0,0 +1,13 @@
+--[[Free VIP Server Script]]--
+--[[Made by Sam_Lord]]--
+--[[Hacked together from code by SIN of the V3RMILLION FORM]]--
+
+id = 10725312 --[[Insert ID of Place HERE]]--
+
+--[[Don't mess with this unless you know what you are doing.]]--
+
+local TS = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local code = TS:ReserveServer(id)
+
+TS:TeleportToPrivateServer(id,code,Players:GetPlayers())
